ff4374b9b50d91d16787e80e37274581d9f9b346
MontCassel <frdhls@gmail.com>
2020-04-03 15:13:02 +1100

    gg

--- spreadsheet-sync/main.js
+++ spreadsheet-sync/main.js
@@ -36,8 +36,8 @@ async function syncWithSheet() {
     ).filter(shape => !!shape.metadata[appId]);
 
     const frame = frames.find(shape => shape.metadata[appId].title === ItemName);
-    const seedframe = frames.find(shape => shape.metadata[appId].title === "SEED");
-    const SeedColor = seedframe.backgroundColor
+    //const seedframe = frames.find(shape => shape.metadata[appId].title === "SEED");
+    //const SeedColor = seedframe.backgroundColor
     
 
     if (frame) {
@@ -56,7 +56,7 @@ async function syncWithSheet() {
           textAlign: "r",
           fontSize: 12,
           borderWidth: 0,
-          
+
           //backgroundColor: SeedColor,
         },
         title: ItemName,
